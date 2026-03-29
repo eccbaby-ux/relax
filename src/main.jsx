161f56fe7e0a0d4f2ba1264bf9a2d88cbc26e575
Shay <eccbaby@gmail.com>
2026-03-29 20:02:52 +0300

    Add PWA support - manifest, service worker, icons for Android & iPhone

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,8 @@
 import { StrictMode } from 'react'
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'));
+}
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
